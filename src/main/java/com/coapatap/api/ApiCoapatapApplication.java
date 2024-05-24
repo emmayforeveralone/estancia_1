f7d77fd211686bf5d9fbf541e0daa88260834882
Emmanuel Garcia <emmanuelgarciacruz987@gmail.com>
2024-05-24 13:57:47 -0600

    Primer commit para la estancia

---
+++ src/main/java/com/coapatap/api/ApiCoapatapApplication.java
@@ -0,0 +1,13 @@
+package com.coapatap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCoapatapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCoapatapApplication.class, args);
+	}
+
+}
